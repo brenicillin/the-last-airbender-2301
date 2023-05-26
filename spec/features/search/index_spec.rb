31502f24ab1e6a3200ef3eef4fd776e10a124459
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-05-26 15:21:22 -0400

    create search index spec

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Search index page', type: :feature do
+  describe 'As a user' do
+    it 'I can search for members of a nation' do
+      visit '/'
+
+      select 'Fire Nation', from: :nation
+      click_button 'Search For Members'
+      save_and_open_page
+      expect(current_path).to eq(search_path)
+      within('#total-members') do
+        expect(page).to have_content('Total Members: 97')
+      end
+
+      within ('#members') do
+        expect(page).to have_content("Name: Afiko")
+        expect(page).to have_content("Allies: Fire Nation")
+        expect(page).to have_content("Enemies: Aang")
+        expect(page).to have_content("Affiliations: Fire Nation")
+      end
+    end
+  end
+end
