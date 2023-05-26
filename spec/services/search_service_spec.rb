77f5c20b58dd171e81b69d3a54a3a69aa9faa508
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-05-26 16:52:32 -0400

    add stubs to service spec

--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe SearchService do
+  before(:each) do
+    stub_request(:get, "https://last-airbender-api.fly.dev/api/v1/characters?affiliation=fire+nation&perPage=120&page=1")
+     .to_return(status: 200, body: File.read('./spec/fixtures/nation_members.json'))
+    stub_request(:get, "https://last-airbender-api.fly.dev/api/v1/characters?affiliation=fire+nation&perPage=25&page=1")
+     .to_return(status: 200, body: File.read('./spec/fixtures/get_members.json'))
+  end
   describe 'class methods' do
     it 'returns all members of a nation' do
       response = SearchService.nation_members("fire+nation")
@@ -10,7 +16,7 @@ RSpec.describe SearchService do
       expect(response.first).to have_key(:enemies)
       expect(response.first).to have_key(:affiliation)
       expect(response.first).to have_key(:name)
-      expect(response.first).to have_key(:photoUrl)
+      expect(response.second).to have_key(:photoUrl)
     end
 
     it 'returns the first 25 members of a nation' do
@@ -22,7 +28,7 @@ RSpec.describe SearchService do
       expect(response.first).to have_key(:enemies)
       expect(response.first).to have_key(:affiliation)
       expect(response.first).to have_key(:name)
-      expect(response.first).to have_key(:photoUrl)
+      expect(response.second).to have_key(:photoUrl)
     end
   end
 end
